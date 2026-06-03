04472d0fba7883edc5a4f97a0995826825216511
bruno rojo <mr.rojo.bruno@gmail.com>
2026-06-03 14:19:20 +0200

    migration ok

--- db/schema.rb
+++ db/schema.rb
@@ -56,6 +56,15 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_03_103920) do
     t.index ["user_id"], name: "index_games_on_user_id"
   end
 
+  create_table "locations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_locations_on_user_id"
+  end
+
   create_table "messages", force: :cascade do |t|
     t.bigint "chat_id", null: false
     t.datetime "created_at", null: false
@@ -229,6 +238,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_03_103920) do
   add_foreign_key "chats", "games"
   add_foreign_key "chats", "users"
   add_foreign_key "games", "users"
+  add_foreign_key "locations", "users"
   add_foreign_key "messages", "chats"
   add_foreign_key "messages", "users"
   add_foreign_key "solid_queue_blocked_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
