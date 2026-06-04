d5166da69bb3e59f10fe31f50664d00c346d0b5b
patrice bernard <patrice.a.bernard@laposte.fr>
2026-06-04 11:34:47 +0200

    cherry picking conflicts fixed 'c5143f2'

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,15 +8,35 @@ class MessagesController < ApplicationController
     @message.user = current_user
 
     if @message.save
-      redirect_to @chat
+      build_conversation_history
+      respond_to do |format|
+        format.turbo_stream # renders `app/views/messages/create.turbo_stream.erb`
+        format.html { redirect_to @chat }
+      end
     else
-      render :index, status: :unprocessable_entity
+      respond_to do |format|
+        format.turbo_stream { render turbo_stream: turbo_stream.update("new_message_container",
+                                                                        partial: "messages/form",
+                                                                        locals: {
+                                                                          chat: @chat,
+                                                                          message: @message })
+                            }
+        format.html { render "chats/show", status: :unprocessable_entity }
+      end
+      # render :index, status: :unprocessable_entity
     end
   end
 
   private
 
+  def build_conversation_history
+    @chat.messages.each do |message|
+      @message.add_message(message)
+    end
+  end
+
   def message_params
     params.require(:message).permit(:content)
   end
+
 end
