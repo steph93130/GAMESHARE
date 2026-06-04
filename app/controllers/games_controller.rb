87dfee2642e6a790734143ae5c301c0bb34c6506
celine deshayes <deshayesceline@outlook.fr>
2026-06-04 16:57:40 +0200

    mapbox-markers done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,22 +1,29 @@
 class GamesController < ApplicationController
   def index
-    @address = params[:query]
-    result = Geocoder.search(@address).first
-    @user_markers = [{ lat: result.latitude, lng: result.longitude }]
+    @games = policy_scope(Game).where(available: true)
+    if params[:query].present?
+      @address = params[:query]
+      result = Geocoder.search(@address).first
+      @user_markers = [{
+        lat: result.latitude,
+        lng: result.longitude,
+        marker_html: render_to_string(partial: "marker_user")
+      }]
 
-    @games = policy_scope(Game).where(available: true).near(@address, 5)
-    # @games = Game.near(address, 0.3)
+      @games = @games.near(@address, 5)
+      # @games = Game.near(address, 0.3)
+    end
     @game_markers = @games.where.not(lat: nil, lng: nil).map do |game|
-      {
-        lat: game.lat,
-        lng: game.lng
-      }
+      { lat: game.lat, lng: game.lng,
+        info_window_html: render_to_string(partial: "info_window", locals: { game: game }),
+        marker_html: render_to_string(partial: "marker_game") }
     end
   end
 
   def show
     @game = Game.find(params[:id])
     authorize @game
+    @address = params[:query]
   end
 
   def new
--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -21,21 +21,31 @@ export default class extends Controller {
   }
   #addMarkersToMap() {
     this.gameMarkersValue.forEach((marker) => {
-      new mapboxgl.Marker()
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
+      const customGameMarker = document.createElement("div")
+      customGameMarker.innerHTML = marker.marker_html
+      new mapboxgl.Marker(customGameMarker)
         .setLngLat([marker.lng, marker.lat])
+        .setPopup(popup)
         .addTo(this.map)
     })
-    this.userMarkersValue.forEach((marker) => {
-      new mapboxgl.Marker()
-        .setLngLat([marker.lng, marker.lat])
-        .addTo(this.map)
-    })
-}
+    if (this.userMarkersValue) {
+      this.userMarkersValue.forEach((marker) => {
+        const customUserMarker = document.createElement("div")
+        customUserMarker.innerHTML = marker.marker_html
+        new mapboxgl.Marker(customUserMarker)
+          .setLngLat([marker.lng, marker.lat])
+          .addTo(this.map)
+      })
+    }
+  }
 
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
     this.gameMarkersValue.forEach(marker => bounds.extend([marker.lng, marker.lat]))
-    this.userMarkersValue.forEach(marker => bounds.extend([marker.lng, marker.lat]))
+    if (this.userMarkersValue) {
+      this.userMarkersValue.forEach(marker => bounds.extend([marker.lng, marker.lat]))
+    }
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 5000 })
   }
 }
