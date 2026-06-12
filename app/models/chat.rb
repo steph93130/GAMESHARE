361a0fc34927145048880e1218ed3861c10c0d9d
celine deshayes <deshayesceline@outlook.fr>
2026-06-12 09:14:19 +0200

    last fix done

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,7 +1,7 @@
 class Chat < ApplicationRecord
   belongs_to :game
   belongs_to :user
-  has_many :messages
+  has_many :messages, dependent: :destroy
   # Fait en sorte de n'avoir n'avoir qu'un seul booking
   has_one :booking
 end
